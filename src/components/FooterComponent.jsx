25452f1f810902c6c011dbc3b17f669b8d7054e0
Subhadeep Ghosh <subhadeep051@icloud.com>
2025-04-04 17:54:14 +0530

    Responsive check

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,19 +1,32 @@
 import { Link } from "react-router-dom";
+import "../styles/FooterComponent.css"; // Custom styles
 
 const FooterComponent = () => {
     return (
-        <footer className="bg-dark text-white text-center p-4"> 
-            <p>&copy; {new Date().getFullYear()} E_Com. All rights reserved.</p>
-            <p>
-                Follow us on:  
-                <a href="#" className="text-white mx-2">Facebook</a> |  
-                <a href="#" className="text-white mx-2">Instagram</a> |  
-                <a href="#" className="text-white mx-2">Twitter</a>
-            </p>
-            {/* Admin Login Button */}
-            <p className="mt-3">
-                <Link to="/admin/login" className="btn btn-outline-light btn-sm">Admin Login</Link>
-            </p>
+        <footer className="footer bg-dark text-white text-center p-4">
+            <div className="container">
+                <div className="row">
+                    {/* Left Section - Branding */}
+                    <div className="col-12 col-md-4">
+                        <p>&copy; {new Date().getFullYear()} <b>E_Com</b>. All rights reserved.</p>
+                    </div>
+
+                    {/* Center Section - Social Media Links */}
+                    <div className="col-12 col-md-4">
+                        <p>Follow us on:</p>
+                        <a href="#" className="text-white mx-2">Facebook</a> |  
+                        <a href="#" className="text-white mx-2">Instagram</a> |  
+                        <a href="#" className="text-white mx-2">Twitter</a>
+                    </div>
+
+                    {/* Right Section - Admin Login */}
+                    <div className="col-12 col-md-4 mt-3 mt-md-0">
+                        <Link to="/admin/login" className="btn btn-outline-light btn-sm">
+                            Admin Login
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 };
